61238158af0caacdf2ec4146a3b02b2bf2cf3733
Filippo Gambarota <filippo.gambarota@gmail.com>
2020-10-09 13:55:33 +0200

    update sim_binomial with random ntrials

--- R/binomial_data_simulation.R
+++ R/binomial_data_simulation.R
@@ -1,22 +1,39 @@
-sim_binomial = function(sample_size, ntrials, p, nsim = 1, sample_p = FALSE, mean_p = 0, sd_p = 0) {
+sim_binomial = function(sample_size, ntrials, p, nsim = 1, sample_p = FALSE, mean_p = 0, sd_p = 0,
+                        sample_trials = T, mean_trial = 0, sd_trial = 0) {
   require(dplyr)
-  
-  sim = lapply(sample_size, function(x) { # sample size level
+
+  if(sample_trials){
+    ntrials = round(rnorm(sample_size, mean_trial, sd_trial)) # get individual trials
     if(sample_p){
-      p = rnorm(x, mean_p, sd_p)
+      p = round(rnorm(sample_size, mean_p, sd_p),2)
     }
-    lapply(ntrials, function(y) { # trials level
-      temp = as.vector(replicate(nsim, expr = rbinom(x, y, p)))
-      temp_sim = data.frame(
-        id = 1:x,
-        success = temp,
-        nsim = rep(1:nsim, each = x),
-        sample_size = x,
-        ntrials = y,
-        p = p)
+    temp = as.vector(replicate(nsim, expr = rbinom(sample_size, ntrials, p)))
+    sim = data.frame(
+      id = 1:sample_size,
+      success = temp,
+      nsim = rep(1:nsim, each = sample_size),
+      sample_size = sample_size,
+      ntrials = ntrials,
+      p = p)
+  }
+  else{
+    sim = lapply(sample_size, function(x) { # sample size level
+      if(sample_p){
+        p = rnorm(x, mean_p, sd_p)
+      }
+      lapply(ntrials, function(y) { # trials level
+        temp = as.vector(replicate(nsim, expr = rbinom(x, y, p)))
+        temp_sim = data.frame(
+          id = 1:x,
+          success = temp,
+          nsim = rep(1:nsim, each = x),
+          sample_size = x,
+          ntrials = y,
+          p = p)
+      })
     })
-  })
-  sim = lapply(sim, function(x){dplyr::bind_rows(x)})
-  sim = dplyr::bind_rows(sim)
+    sim = lapply(sim, function(x){dplyr::bind_rows(x)})
+    sim = dplyr::bind_rows(sim)
+  }
   return(sim)
 }
