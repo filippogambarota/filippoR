812990c6ff0e568bc2264766a5cc2d7be750a0c8
Filippo Gambarota <filippo.gambarota@gmail.com>
2020-10-31 19:31:26 +0100

    implementing data.dable in bin_to_bern

--- R/bin_to_bern.R
+++ R/bin_to_bern.R
@@ -1,29 +1,23 @@
 bin_to_bern <- function(data_bin){
-  
+
   success = lapply(data_bin$success, function(x){
     rep(1, x)
   })
-  
-  fail = lapply(data_bin$ntrials - data_bin$success, function(x){
+
+  fail = lapply(data_bin$fail, function(x){
     rep(0, x)
   })
-  
+
   acc = unlist(mapply(c, success, fail, SIMPLIFY=FALSE))
-  
-  data_bern = slice(data_bin, rep(1:length(data_bin$id), data_bin$ntrials))
-  
+
+  data_bin <- lazy_dt(data_bin)
+
+  data_bern <- data_bin %>%
+    slice(rep(1:length(id), ntrials)) %>%
+    as.data.table() %>%
+    as_tibble()
+
   data_bern$acc = acc
-  
+
   return(data_bern)
 }
-
-
-
-
-
-
-
-
-
-
-
