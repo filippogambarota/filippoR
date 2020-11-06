6abaacf3ca5a5e72b2e50b526f0c75353c2cd73a
Filippo Gambarota <filippo.gambarota@gmail.com>
2020-11-06 13:04:07 +0100

    update function with parallel processing and progress bar

--- R/binomial_data_simulation.R
+++ R/binomial_data_simulation.R
@@ -1,92 +1,93 @@
 sim_binomial <- function(sample_size, ntrials, p, nsim = 1, sample_p = FALSE, sample_trials = FALSE,
-                       dist_trial = "norm", dist_p = "norm",
-                       mean_p = 0, sd_p = 0, mean_trial = 0, sd_trial = 0, min_unif = 0, max_unif = 0){
+                         dist_trial = "norm", dist_p = "norm",
+                         mean_p = 0, sd_p = 0, mean_trial = 0, sd_trial = 0, min_unif = 0, max_unif = 0,
+                         parallel = F){
 
-# Creating Dataframe with conditions
+  # Creating Dataframe with conditions
 
-nsim <- 1:nsim
+  nsim <- 1:nsim
 
-if(sample_trials & !sample_p){
+  if(sample_trials & !sample_p){
 
-  dat <- expand_grid(
-    sample_size,
-    p
-  )
+    dat <- expand_grid(
+      sample_size,
+      p
+    )
 
-  id <- unlist(lapply(dat$sample_size, function(x) 1:x))
+    id <- unlist(lapply(dat$sample_size, function(x) 1:x))
 
-  dat <- slice(dat, rep(1:n(), dat$sample_size))
+    dat <- slice(dat, rep(1:n(), dat$sample_size))
 
-  dat$id <- id
+    dat$id <- id
 
-  dat <- expand_grid(
-    dat,
-    nsim
-  )
+    dat <- expand_grid(
+      dat,
+      nsim
+    )
 
-  if(dist_trial == "norm"){
-    dat$ntrials <- round(rnorm(nrow(dat), mean_trial, sd_trial))
+    if(dist_trial == "norm"){
+      dat$ntrials <- round(rnorm(nrow(dat), mean_trial, sd_trial))
+    }
+    else if(dist_trial == "unif"){
+      dat$ntrials <- round(runif(x, min_unif_trial, max_unif_trial))
+    }
+
   }
-  else if(dist_trial == "unif"){
-    dat$ntrials <- round(runif(x, min_unif_trial, max_unif_trial))
-  }
+  else if(sample_p & !sample_trials){
 
-}
-else if(sample_p & !sample_trials){
+    dat <- expand_grid(
+      sample_size,
+      ntrials
+    )
 
-  dat <- expand_grid(
-    sample_size,
-    ntrials
-  )
+    id <- unlist(lapply(dat$sample_size, function(x) 1:x))
 
-  id <- unlist(lapply(dat$sample_size, function(x) 1:x))
+    dat <- slice(dat, rep(1:n(), dat$sample_size))
 
-  dat <- slice(dat, rep(1:n(), dat$sample_size))
+    dat$id <- id
 
-  dat$id <- id
+    dat <- expand_grid(
+      dat,
+      nsim
+    )
 
-  dat <- expand_grid(
-    dat,
-    nsim
-  )
-
-  if(dist_p == "norm"){
-    dat$p <- round(rnorm(nrow(dat), mean_p, sd_p), 3)
-  }
-  else if(dist_trial == "unif"){
-    dat$p <- round(runif(x, min_unif_p, max_unif_p), 3)
-  }
-}
-
-else if(sample_trials & sample_p){
-
-  dat <- expand_grid(
-    sample_size,
-    nsim
-  )
-
-  id <- unlist(lapply(dat$sample_size, function(x) 1:x))
-
-  dat <- slice(dat, rep(1:n(), dat$sample_size))
-
-  dat$id <- id
-
-  if(dist_trial == "norm"){
-    dat$ntrials <- round(rnorm(nrow(dat), mean_trial, sd_trial))
-  }
-  else if(dist_trial == "unif"){
-    dat$ntrials <- round(runif(x, min_unif_trial, max_unif_trial))
+    if(dist_p == "norm"){
+      dat$p <- round(rnorm(nrow(dat), mean_p, sd_p), 3)
+    }
+    else if(dist_trial == "unif"){
+      dat$p <- round(runif(x, min_unif_p, max_unif_p), 3)
+    }
   }
 
-  if(dist_p == "norm"){
-    dat$p <- round(rnorm(nrow(dat), mean_p, sd_p), 3)
-  }
-  else if(dist_p == "unif"){
-    dat$p <- round(runif(x, min_unif_p, max_unif_p), 3)
-  }
-}
+  else if(sample_trials & sample_p){
 
-else if(!sample_trials & !sample_p){
+    dat <- expand_grid(
+      sample_size,
+      nsim
+    )
+
+    id <- unlist(lapply(dat$sample_size, function(x) 1:x))
+
+    dat <- slice(dat, rep(1:n(), dat$sample_size))
+
+    dat$id <- id
+
+    if(dist_trial == "norm"){
+      dat$ntrials <- round(rnorm(nrow(dat), mean_trial, sd_trial))
+    }
+    else if(dist_trial == "unif"){
+      dat$ntrials <- round(runif(x, min_unif_trial, max_unif_trial))
+    }
+
+    if(dist_p == "norm"){
+      dat$p <- round(rnorm(nrow(dat), mean_p, sd_p), 3)
+    }
+    else if(dist_p == "unif"){
+      dat$p <- round(runif(x, min_unif_p, max_unif_p), 3)
+    }
+  }
+
+  else if(!sample_trials & !sample_p){
 
     dat <- expand_grid(
       sample_size,
@@ -105,7 +106,15 @@ else if(!sample_trials & !sample_p){
 
   # Simulating Binomial Data
 
-  dat$success <- mapply(function(x, y) rbinom(1, x, y), dat$ntrials, dat$p)
+  if(parallel){
+    warning("using plan(multicore)")
+    plan(multicore)
+    dat$success <- future_mapply(function(x, y) rbinom(1, x, y), dat$ntrials, dat$p,
+                                 future.seed = T)
+  }
+  else{
+    dat$success <- pbmapply(function(x, y) rbinom(1, x, y), dat$ntrials, dat$p)
+  }
 
   # Cleaning dataset
 
